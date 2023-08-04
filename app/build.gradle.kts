6dfa06582630cc26b63f8fd6ec5d52504cee03c3
Shivansh Kumar <codes.stalker@gmail.com>
2023-08-05 02:09:20 +0530

    Final COmmit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ android {
     namespace = "com.example.cameraapp"
     compileSdk = 33
 
+
     defaultConfig {
         applicationId = "com.example.cameraapp"
         minSdk = 24
@@ -21,8 +22,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -33,18 +33,28 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("androidx.camera.camera2:camera2:1.1.0-alpha01")
-    implementation ("androidx.camera.view:camera-view:1.1.0-alpha04")
+
+
+
+    implementation("androidx.camera:camera-view:1.2.3")
+    implementation("androidx.camera:camera-extensions:1.2.3")
+
 
 }
